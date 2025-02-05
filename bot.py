79a01f0b419ab9dac497fa873dfd4283105ff487
SuperClicheUsername <makoziol0@gmail.com>
2025-02-04 19:06:26 -0500

    Remove a step in logging setup

--- bot.py
+++ bot.py
@@ -45,10 +45,10 @@ def setup_logging():
         config = json.load(f_in)
 
     logging.config.dictConfig(config)
-    queue_handler = logging.getHandlerByName("queue_handler")
-    if queue_handler is not None:
-        queue_handler.listener.start()
-        atexit.register(queue_handler.listener.stop)
+    # queue_handler = logging.getHandlerByName("queue_handler")
+    # if queue_handler is not None:
+    #     queue_handler.listener.start()
+    #     atexit.register(queue_handler.listener.stop)
 
 
 def savedata():
